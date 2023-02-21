0a1996a752dcc40065ecb27d22eab178de386dc5
MiulesiKulasekara <it21028878@my.sliit.lk>
2023-02-21 12:21:41 +0530

    Accessing data that in the same file

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -3,6 +3,29 @@
 
 import Main from "./Components/Main";
 
+const details = [
+  {
+    name: "Pasan",
+    position: "doctor",
+    city: "Galle",
+  },
+  {
+    name: "Nimali",
+    position: "teacher",
+    city: "Badulla",
+  },
+  {
+    name: "Selvaraja",
+    position: "shop owner",
+    city: "Trincomalee",
+  },
+  {
+    name: "Ruwan",
+    position: "ceo",
+    city: "colombo",
+  },
+];
+
 function App() {
   return (
     //One jsx element is retured by return statemet. Multiple element can not be used. But inside one element , It is posible to use multiple elements.
@@ -16,21 +39,36 @@ function App() {
 
     <div className="App">
       <p>My name is shamal Aravinda</p>
-      <Main />
-      <Main />
-  
-      <Main>
+
+      <Main></Main>
+
+      {/* -----data accessing -> data in the same file----- */}
+
+      {details.map((element , index) => {
+        return (
+          <Main
+            key={index}
+            item1={element.name}
+            item2={element.position}
+            item3={element.city}
+          />
+        );
+      })}
+
+      {/* <Main />
+      <Main item1 ="aaaa" item2="bbb" item3="ccc"/>
+      <Main /> */}
+
+      {/* <Main>
         <h5>Hello</h5>
         <p>Hi friends</p>
-      </Main>
+      </Main> */}
 
       {/* We can acsess elements inside a component as children using props */}
 
-      <Main></Main>
-      <Main item1 ="aaaa" item2="bbb" item3="ccc"/>
-     
+      {/* <Main></Main>
+      <Main item1 ="aaaa" item2="bbb" item3="ccc"/> */}
 
-      
       {/* (shift + alt + down arrow) for duplicate a particular item */}
     </div>
   );
--- myreactapp/src/Components/Main.js
+++ myreactapp/src/Components/Main.js
@@ -1,34 +1,42 @@
-const degree = 'BSC IT special in SE';
-const job = 'Lecturer';
+// const degree = "BSC IT special in SE";
+// const job = "Lecturer";
 
-const myObject = {
-    degree: `${degree}`,
-    job: `${job}`,
-    city: 'Colombo'
-}
+// const myObject = {
+//   degree: `${degree}`,
+//   job: `${job}`,
+//   city: "Colombo",
+// };
 
-function Main({children , item1 , item2 , item3}) {
+function Main({ children, item1, item2, item3 }) {
+  // props(properties) ==> comes as objects
+  // Therefore we can use childeren keyword instead of props keword. This is relevnets to object destructering
+  // console.log(props)
 
-    // props(properties) ==> comes as objects
-    // Therefore we can use childeren keyword instead of props keword. This is relevnets to object destructering
-    // console.log(props);
+  // const { city: c } = myObject;
+  // const {city} = myObject;
 
-    const {city} = myObject;
-    
   return (
     <>
       <p>I am 22 years old.</p>
-      {children}
+      <ul>
+        <li>{item1}</li>
+        <li>{item2}</li>
+        <li>{item3}</li>
+      </ul>
+
+      {/* Maps for loop a arry with a return value */}
+
+      {/* {children}
       <ul>
         <li>{job}</li>
         <li>{degree}</li>
         <li>{myObject.degree}</li>
         <li>{myObject.job}</li>
-        <li>{city}</li>
+        <li>{c}</li>
         <li>{item1}</li>
         <li>{item2}</li>
         <li>{item3}</li>
-      </ul>
+      </ul> */}
     </>
   );
 }
@@ -36,3 +44,5 @@ function Main({children , item1 , item2 , item3}) {
 export default Main;
 
 //export default functionName
+
+// <></> is a react fragment
